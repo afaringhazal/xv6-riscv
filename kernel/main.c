452afe73c61ef6099a153c0419f6b3cf86cd24e3
afaringhazal <afaringhazal@gmail.com>
2022-11-24 00:35:43 +0330

    add kfreemem() in kalloc.c and define property in defs.h in kernel , When running xv6, we display a report of free memory

--- kernel/main.c
+++ kernel/main.c
@@ -17,6 +17,7 @@ main()
     printf("xv6 kernel is booting\n");
     printf("\n");
     kinit();         // physical page allocator
+    printf("xv6 free pages: %d\n", kfreemem()); //When running xv6, we display a report of free memory
     kvminit();       // create kernel page table
     kvminithart();   // turn on paging
     procinit();      // process table
